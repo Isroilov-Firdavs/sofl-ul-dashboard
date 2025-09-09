345fb083ccf03cf81222963cdd8271a1b93d13cc
Isroilov Firdavs <firdavsisroilov28@gmail.com>
2025-09-09 23:21:03 +0500

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\CitiesSeeder;
 use Database\Seeders\RegionsSeeder;
+use Database\Seeders\YesOrNoSeeder;
+use Database\Seeders\PackagingSeeder;
+use Database\Seeders\StatusListSeeder;
+use Database\Seeders\TypesOfShipmentsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +22,13 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-            RegionsSeeder::class
+            RegionsSeeder::class,
+            CitiesSeeder::class,
+            PackagingSeeder::class,
+            RegionsSeeder::class,
+            StatusListSeeder::class,
+            TypesOfShipmentsSeeder::class,
+            YesOrNoSeeder::class,
         ]);
     }
 }
